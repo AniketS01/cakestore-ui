4d6693396f8639df4515b93e93ec67682df0ccce
AniketS01 <aniketshetyeajs@gmail.com>
2021-11-12 15:32:50 +0530

    firebase done

--- src/Pages/BirthdayCake.jsx
+++ src/Pages/BirthdayCake.jsx
@@ -1,33 +1,73 @@
-import React from "react"
-import { TrashFill,BagPlusFill } from "react-bootstrap-icons";
-import img from '.././components/img/cakeshow2.jpg'
-const BirthdayCake = () => {
-  return (
-      <>
-<div>
-    <h1 className="text-center mt-3 m-4" style={{backgroundColor:"#C3404E",color:"white",fontFamily:"'Hurricane', cursive"}}>Birthday Cakes</h1>
-</div>
-          
-      
-    <div className="row container ">
-      <div className="col-lg-3 col-md-6 col-sm-6 ">
-        <div className="card shadow  mb-5 bg-body rounded">
-          <img className="img-fluid card-img-top card-image" src={img} />
+import React, { useEffect, useState } from 'react';
+import { TrashFill, BagPlusFill } from 'react-bootstrap-icons';
+import img from '.././components/img/cakeshow2.jpg';
+import { db } from '../firebase';
 
-          <div className="card-body p-1">
-            <h1 className="card-heading">Black Forest</h1>
-            <p className="card-text">Nice cake for birthday's</p>
-            <div
-              className="card-text d-flex justify-content-between align-items-center"
-              style={{ fontWeight: 600 }}
-            >
-              Rs 200/-
-              <button className="btn btn-sm btn-outline-danger">Buy</button>
+const BirthdayCake = () => {
+  const [BirthdayCake, setBirthdayCake] = useState([]);
+
+  useEffect(() => {
+    db.collection('Cakes')
+      .get()
+      .then((snapshot) => {
+        const Dcake = [];
+        snapshot.forEach((doc) => {
+          const data = {
+            id: doc.id,
+            img_id: doc.data().clourinary_id,
+            name: doc.data().name,
+            image: doc.data().image,
+            category: doc.data().Category,
+            description: doc.data().description,
+            price: doc.data().price,
+          };
+          if (data.category === 'Birthday Cake') {
+            Dcake.push(data);
+          }
+        });
+        setBirthdayCake(Dcake);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1
+          className="text-center mt-3 m-4"
+          style={{
+            backgroundColor: '#C3404E',
+            color: 'white',
+            fontFamily: "'Hurricane', cursive",
+          }}
+        >
+          Birthday Cakes
+        </h1>
+      </div>
+
+      <div className="row container ">
+        {BirthdayCake.map((cake) => (
+          <div className="col-lg-3 col-md-6 col-sm-6 ">
+            <div className="card shadow  mb-5 bg-body rounded">
+              <img
+                className="img-fluid card-img-top card-image"
+                src={cake.image}
+              />
+
+              <div className="card-body p-1">
+                <h1 className="card-heading">{cake.name}</h1>
+                <p className="card-text">{cake.description}</p>
+                <div
+                  className="card-text d-flex justify-content-between align-items-center"
+                  style={{ fontWeight: 600 }}
+                >
+                  Rs {cake.price}/-
+                  <button className="btn btn-sm btn-outline-danger">Buy</button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
-    </div>
     </>
   );
 };
--- src/Pages/OccasionalCakes.jsx
+++ src/Pages/OccasionalCakes.jsx
@@ -1,36 +1,75 @@
-import React from 'react'
-import { TrashFill,BagPlusFill } from "react-bootstrap-icons";
-import img from '.././components/img/cakeshow3.jpg'
+import React, { useState, useEffect } from 'react';
+import { TrashFill, BagPlusFill } from 'react-bootstrap-icons';
+import img from '.././components/img/cakeshow3.jpg';
+import { db } from '../firebase';
+
 const OccasionalCakes = () => {
-    return (
-        <>
-        <div>
-            <h1 className="text-center mt-3 m-4" style={{backgroundColor:"#C3404E",color:"white",fontFamily:"'Hurricane', cursive"
-}}>Occasional Cakes</h1>
-        </div>
-                  
-              
-            <div className="row container ">
-              <div className="col-lg-3 col-md-6 col-sm-6 ">
-                <div className="card shadow  mb-5 bg-body rounded">
-                  <img className="img-fluid card-img-top card-image" src={img} />
-        
-                  <div className="card-body p-1">
-                    <h1 className="card-heading">Black Forest</h1>
-                    <p className="card-text">Nice cake for birthday's</p>
-                    <div
-                      className="card-text d-flex justify-content-between align-items-center"
-                      style={{ fontWeight: 600 }}
-                    >
-                      Rs 200/-
-                      <button className="btn btn-sm btn-outline-danger">Buy</button>
-                    </div>
-                  </div>
+  const [festivalCake, setFestivalCake] = useState([]);
+
+  useEffect(() => {
+    db.collection('Cakes')
+      .get()
+      .then((snapshot) => {
+        const Dcake = [];
+        snapshot.forEach((doc) => {
+          const data = {
+            id: doc.id,
+            img_id: doc.data().clourinary_id,
+            name: doc.data().name,
+            image: doc.data().image,
+            category: doc.data().Category,
+            description: doc.data().description,
+            price: doc.data().price,
+          };
+          if (data.category === 'Festival cake') {
+            Dcake.push(data);
+          }
+        });
+        setFestivalCake(Dcake);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1
+          className="text-center mt-3 m-4"
+          style={{
+            backgroundColor: '#C3404E',
+            color: 'white',
+            fontFamily: "'Hurricane', cursive",
+          }}
+        >
+          Occasional Cakes
+        </h1>
+      </div>
+
+      <div className="row container ">
+        {festivalCake.map((cake) => (
+          <div className="col-lg-3 col-md-6 col-sm-6 ">
+            <div className="card shadow  mb-5 bg-body rounded">
+              <img
+                className="img-fluid card-img-top card-image"
+                src={cake.image}
+              />
+
+              <div className="card-body p-1">
+                <h1 className="card-heading">{cake.name}</h1>
+                <p className="card-text">{cake.description}</p>
+                <div
+                  className="card-text d-flex justify-content-between align-items-center"
+                  style={{ fontWeight: 600 }}
+                >
+                  Rs {cake.price}/-
+                  <button className="btn btn-sm btn-outline-danger">Buy</button>
                 </div>
               </div>
             </div>
-            </>
-    )
-}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default OccasionalCakes
+export default OccasionalCakes;
