ca8faf2e391d44ee7d9fbc6f73722b5edb738173
AniketS01 <aniketshetyeajs@gmail.com>
2021-11-25 19:57:31 +0530

    payment getaway

--- src/Pages/BestSellers.jsx
+++ src/Pages/BestSellers.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { TrashFill, BagPlusFill } from 'react-bootstrap-icons';
-import img from '.././components/img/cakeshow1.jpg';
-import { db } from '../firebase';
+import React, { useState, useEffect } from "react";
+import { TrashFill, BagPlusFill } from "react-bootstrap-icons";
+import img from ".././components/img/cakeshow1.jpg";
+import { db } from "../firebase";
+import { payment } from "../payment/Payment";
 
 const BestSellers = () => {
   const [DesignerCakes, setDesignerCakes] = useState([]);
 
   useEffect(() => {
-    db.collection('Cakes')
+    db.collection("Cakes")
       .get()
       .then((snapshot) => {
         const Dcake = [];
@@ -21,7 +22,7 @@ const BestSellers = () => {
             description: doc.data().description,
             price: doc.data().price,
           };
-          if (data.category === 'Designer cake') {
+          if (data.category === "Designer cake") {
             Dcake.push(data);
           }
         });
@@ -34,10 +35,10 @@ const BestSellers = () => {
     <>
       <div>
         <h1
-          className="text-center mt-3 m-4"
+          className='text-center mt-3 m-4'
           style={{
-            backgroundColor: '#C3404E',
-            color: 'white',
+            backgroundColor: "#C3404E",
+            color: "white",
             fontFamily: "'Hurricane', cursive",
           }}
         >
@@ -45,24 +46,29 @@ const BestSellers = () => {
         </h1>
       </div>
 
-      <div className="row container ">
+      <div className='row container '>
         {DesignerCakes.map((cake) => (
-          <div className="col-lg-3 col-md-6 col-sm-6 ">
-            <div className="card shadow  mb-5 bg-body rounded">
+          <div className='col-lg-3 col-md-6 col-sm-6 '>
+            <div className='card shadow  mb-5 bg-body rounded'>
               <img
-                className="img-fluid card-img-top card-image"
+                className='img-fluid card-img-top card-image'
                 src={cake.image}
               />
 
-              <div className="card-body p-1">
-                <h1 className="card-heading">{cake.name}</h1>
-                <p className="card-text">{cake.description}</p>
+              <div className='card-body p-1'>
+                <h1 className='card-heading'>{cake.name}</h1>
+                <p className='card-text'>{cake.description}</p>
                 <div
-                  className="card-text d-flex justify-content-between align-items-center "
+                  className='card-text d-flex justify-content-between align-items-center '
                   style={{ fontWeight: 600 }}
                 >
                   Rs {cake.price}/-
-                  <button className="btn btn-sm btn-outline-danger">Buy</button>
+                  <button
+                    className='btn btn-sm btn-outline-danger'
+                    onClick={() => payment(cake.price)}
+                  >
+                    Buy
+                  </button>
                 </div>
               </div>
             </div>
---
+++ src/payment/Payment.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const payment = async (price) => {
+  const response = await axios.post("http://localhost:5000/payment", {
+    price: price,
+  });
+  const { data } = response;
+  const options = {
+    key: "rzp_test_t7PMlIj8PD3BeR",
+    name: "Cake",
+    description: "thanks for ordering",
+    order_id: data.id,
+    handler: async (res) => {
+      try {
+        const paymentId = response.razorpay_payment_id;
+        const url = `http://localhost:5000/capture/${paymentId}`;
+        const captureRes = await axios.post(url, {
+          price: price * 100,
+        });
+        const success = JSON.parse(captureRes.data);
+        if (success.captured) {
+          console.log("success");
+        } else {
+          console.log("failed");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    prefill: {
+      name: "Aniket Shetye",
+      email: "aniketshetyeajs@gmail.com",
+      contact: "9921565101",
+    },
+    theme: {
+      color: "#686CFD",
+    },
+  };
+  const rzpl = new window.Razorpay(options);
+  rzpl.open();
+};
+
+export { payment };
