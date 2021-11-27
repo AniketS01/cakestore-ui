e68514aeace67af0b9722bf46f6e6839633ce724
unknown <bhaleraoulhas01@gmail.com>
2021-11-27 19:29:23 +0530

    info added to all pages

--- src/Pages/BestSellers.jsx
+++ src/Pages/BestSellers.jsx
@@ -78,7 +78,7 @@ const BestSellers = () => {
                   Rs {cake.price}/-
                   <button
                     type="button"
-                    class="btn btn-danger"
+                    className="btn btn-danger"
                     data-bs-toggle="modal"
                     data-bs-target="#exampleModal"
                   >
@@ -86,70 +86,70 @@ const BestSellers = () => {
                   </button>
                   {/* popup window */}
                   <div
-                    class="modal fade"
+                    className="modal fade"
                     id="exampleModal"
                     tabindex="-1"
                     aria-labelledby="exampleModalLabel"
                     aria-hidden="true"
                   >
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLabel">
+                    <div className="modal-dialog">
+                      <div className="modal-content">
+                        <div className="modal-header">
+                          <h5 className="modal-title" id="exampleModalLabel">
                             Enter Information for payment invoice
                           </h5>
                           <button
                             type="button"
-                            class="btn-close"
+                            className="btn-close"
                             data-bs-dismiss="modal"
                             aria-label="Close"
                           ></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body ">
                           <form>
-                          <div class="mb-3">
+                          <div className="mb-3 ">
                               <label
                                 for="exampleInputPassword1"
-                                class="form-label"
+                                className="form-label"
                               >
                                Enter your name
                               </label>
                               <input
                                 type="text"
-                                class="form-control"
-                                id="exampleInputPassword1"
+                                className="form-control "
+                                
                                 ref={nameRef}
                               />
                             </div>
-                            <div class="mb-3">
+                            <div className="mb-3 ">
                               <label
                                 for="exampleInputEmail1"
-                                class="form-label"
+                                className="form-label"
                               >
                                 Email address
                               </label>
                               <input
                                 type="email"
-                                class="form-control"
+                                className="form-control"
                                 id="exampleInputEmail1"
                                 aria-describedby="emailHelp"
                                 ref={emailRef}
                               />
-                              <div id="emailHelp" class="form-text">
+                              <div id="emailHelp" className="form-text">
                                 We'll never share your email with anyone else.
                               </div>
                             </div>
-                            <div class="mb-3">
+                            <div className="mb-3">
                               <label
                                 for="exampleInputPassword1"
-                                class="form-label"
+                                className="form-label"
                               >
                                 Phone Number
                               </label>
                               <input
                                 type="text"
-                                class="form-control"
-                                id="exampleInputPassword1"
+                                className="form-control"
+                                
                                 ref={phoneRef}
                               />
                             </div>
@@ -157,11 +157,11 @@ const BestSellers = () => {
                            
                           </form>
                         </div>
-                        <div class="modal-footer">
+                        <div className="modal-footer">
                           
                           <button
                             type="button"
-                            class="btn btn-danger"
+                            className="btn btn-danger"
                             onClick={() => payment(cake.price, cake.name)}
                           >
                             Make Payment
--- src/Pages/BirthdayCake.jsx
+++ src/Pages/BirthdayCake.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState ,useRef} from "react";
 import { TrashFill, BagPlusFill } from "react-bootstrap-icons";
 import img from ".././components/img/cakeshow2.jpg";
 import { db } from "../firebase";
@@ -6,6 +6,9 @@ import { payment } from "../payment/Payment";
 import "./cards.css";
 const BirthdayCake = () => {
   const [BirthdayCake, setBirthdayCake] = useState([]);
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const phoneRef = useRef()
 
   useEffect(() => {
     db.collection("Cakes")
@@ -63,11 +66,99 @@ const BirthdayCake = () => {
                 >
                   Rs {cake.price}/-
                   <button
-                    className='btn btn-sm btn-outline-danger'
-                    onClick={() => payment(cake.price, cake.name)}
+                    type="button"
+                    className="btn btn-danger"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
                   >
                     Buy
                   </button>
+                  {/* popup */}
+                  <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabindex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog">
+                      <div className="modal-content">
+                        <div className="modal-header">
+                          <h5 className="modal-title" id="exampleModalLabel">
+                            Enter Information for payment invoice
+                          </h5>
+                          <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                          ></button>
+                        </div>
+                        <div className="modal-body">
+                          <form>
+                          <div className="mb-3">
+                              <label
+                                for="exampleInputPassword1"
+                                className="form-label"
+                              >
+                               Enter your name
+                              </label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                id="exampleInputPassword1"
+                                ref={nameRef}
+                              />
+                            </div>
+                            <div className="mb-3">
+                              <label
+                                for="exampleInputEmail1"
+                                className="form-label"
+                              >
+                                Email address
+                              </label>
+                              <input
+                                type="email"
+                                className="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                ref={emailRef}
+                              />
+                              <div id="emailHelp" className="form-text">
+                                We'll never share your email with anyone else.
+                              </div>
+                            </div>
+                            <div className="mb-3">
+                              <label
+                                for="exampleInputPassword1"
+                                className="form-label"
+                              >
+                                Phone Number
+                              </label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                id="exampleInputPassword1"
+                                ref={phoneRef}
+                              />
+                            </div>
+                            
+                           
+                          </form>
+                        </div>
+                        <div className="modal-footer">
+                          
+                          <button
+                            type="button"
+                            className="btn btn-danger"
+                            onClick={() => payment(cake.price, cake.name)}
+                          >
+                            Make Payment
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
